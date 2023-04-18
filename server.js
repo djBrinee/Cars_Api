11b72c4eea121a7ffb544f74168438009c1cf00f
djBrinee <deivyjunior12@gmail.com>
2023-04-18 02:26:17 -0400

    host base de datos

--- server.js
+++ server.js
@@ -1,38 +1,37 @@
 // server cofiguration
 
-const express = require('express')
-const mysql = require('mysql')
-const myconn = require('express-myconnection')
-const routes = require('./routes')
+const express = require('express');
+const mysql = require('mysql');
+const myconn = require('express-myconnection');
+const routes = require('./routes');
 
-
-const app = express()
+const app = express();
 app.set('port', process.env.PORT || 9001);
+
 const dbOptions = {
-    host: 'localhost',
+    host: 'aws.connect.psdb.cloud',
+    user: '5tdxwgerjwwy1pujqweb',
+    password: 'pscale_pw_Wi0b9hMssGaL6zzP5veFsXYMwYoykTkkDOIvmoTB4m1',
+    database: 'dbcars',
     port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'sample_api'
-}
+    ssl: {
+        ca: 'BaltimoreCyberTrustRoot.crt.pem',
+        rejectUnauthorized: false
+    }
+};
 
 // middlewares
-
 app.use(myconn(mysql, dbOptions, 'single'));
-app.use(express.json())
-
+app.use(express.json());
 
 // routes
-app.get('/', (req, res) =>{
-    res.send('Welcome to my API')
-})
-
-app.use('/api', routes)
-
-// server running 
-app.listen(app.get('port'), ()=>{
-    console.log('Server running on port', app.get('port'));
+app.get('/', (req, res) => {
+    res.send('Welcome to my API');
 });
 
+app.use('/api', routes);
 
-
+// server running 
+app.listen(app.get('port'), () => {
+    console.log('Server running on port', app.get('port'));
+});
